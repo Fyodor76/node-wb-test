e0c23031516c9398d08f9ca2e7e62e15fe7c8098
Fyodor76 <100220038+Fyodor76@users.noreply.github.com>
2024-05-16 16:35:19 +0300

    обновлён .env

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+require('dotenv').config();
+
+const url = process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : process.env.DEVELOPMENT_URL;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`App listening on port at ${url}`);
+});
